e1a79c02681a657203bbcefe715b2a6954c30cfe
luis salinas <89706382+lpeniche7@users.noreply.github.com>
2021-12-24 09:01:41 -0500

    Add files via upload

---
+++ lib/config/config.dart
@@ -0,0 +1,14 @@
+// para obtener esta información, crear una app web en el proyecto de firebase console
+// los valores se pueden encontrar en la configuración de la misma
+
+class Configurations {
+  static const apiKey = "AIzaSyBxexfu_UKzkdnMjmKkpMLrkgI15sXlAXI";
+  static const authDomain = "nivelacionciclo4movil.firebaseapp.com";
+  static const projectId = "nivelacionciclo4movil";
+  static const storageBucket = "nivelacionciclo4movil.appspot.com";
+  static const messagingSenderId = "100836913147";
+  static const appId = "1:100836913147:web:774163bb29e73c488c255e";
+  static const databaseURL =
+      "https://nivelacionciclo4movil-default-rtdb.firebaseio.com";
+  static const measurementId = "G-02B4V49N1P";
+}
